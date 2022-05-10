f364e8ec1554b6162624ffebb04c8e2d3da0c1bd
Your Name <you@example.com>
2022-05-10 14:10:12 +0700

    add file cs

---
+++ MyClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1_12
+{
+    //Class
+    class MyClass
+    {
+        public string myField = string.Empty;
+
+        public MyClass()
+        {
+        }
+
+        public void MyMethod(int parameter1, string parameter2)
+        {
+            Console.WriteLine("First Parameter {0}, second parameter {1}",
+                                                        parameter1, parameter2);
+        }
+
+        public int MyAutoImplementedProperty { get; set; }
+
+        private int _myPropertyVar;
+
+        public int MyProperty
+        {
+            get
+            {
+                return _myPropertyVar / 2;
+            }
+
+            set
+            {
+                if (value > 100)
+                    _myPropertyVar = 100;
+                else
+                    _myPropertyVar = value; ;
+            }
+        }
+    }
+}
